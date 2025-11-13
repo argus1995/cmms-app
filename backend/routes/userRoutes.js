2207a875245726b5b6d28e65a9ae3b2718cf0fb0
argus1995 <ariefagus1995@gmail.com>
2025-11-13 10:58:10 +0700

    feat: initialize database connection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/register");
+router.post("/register", (req, res) =>
+  res.json({ message: "This is register" })
+);
+router.post("/login", (req, res) => res.json({ message: "This is login" }));
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+});
